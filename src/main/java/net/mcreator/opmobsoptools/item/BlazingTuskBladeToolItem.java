a582f9ceae86d00e574dc734bb3a493603426596
AnshSharma07 <theboyzgamerz55@gmail.com>
2026-03-01 17:55:12 +0530

    Fix item import packages for 1.21.8 mappings

--- src/main/java/net/mcreator/opmobsoptools/item/BlazingTuskBladeToolItem.java
+++ src/main/java/net/mcreator/opmobsoptools/item/BlazingTuskBladeToolItem.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.item.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.SmallFireball;
--- src/main/java/net/mcreator/opmobsoptools/item/EarthBreakerHammerToolItem.java
+++ src/main/java/net/mcreator/opmobsoptools/item/EarthBreakerHammerToolItem.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.item.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
